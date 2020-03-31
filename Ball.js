46f487015077a18b25be514be0261be76af8972b
Jesus Aguilar <jaguilargutierrez.1294@gmail.com>
2020-03-31 14:53:16 -0700

    first commit

---
+++ Ball.js
@@ -0,0 +1,75 @@
+class Ball{
+	
+	constructor(x,y,c,ww,wh,id){
+		
+		this.x=x
+		this.y=y
+		this.c=c
+		this.ww=ww
+		this.wh=wh
+		this.id=id
+		this.spx= 4 * this.direction()
+		this.spy= 4 * this.direction()
+	}
+
+
+	draw(){
+		push()
+		noStroke()
+		fill(this.c)
+
+
+		ellipse(this.x,this.y,25,25)
+		pop()
+	}
+
+	move(){
+		if (this.x <= 0) {
+			this.spx=this.spx*-1
+		}else if (this.x >= this.ww) {
+			this.spx=this.spx*-1
+		}
+
+		if (this.y <= 0) {
+			this.spy=this.spy*-1
+		}else if (this.y >= this.wh) {
+			this.spy=this.spy*-1
+		}
+
+
+
+		
+		this.x+=this.spx
+		this.y+=this.spy
+	}
+
+	changeDirection(){
+		this.spx=this.spx*-1
+		this.spy=this.spy*-1
+	}
+
+	direction(){
+		
+		return floor(random(2))* 2 - 1
+
+	}
+
+	get getspeedX(){
+
+	}
+	get pX() {
+    	return this.x;
+  	}
+
+  	get pY() {
+    	return this.y;
+  	}
+
+  	get getId() {
+    	return this.id;
+  	}
+
+  	get color() {
+    	return this.c;
+  	}
+}
---
+++ Sketch.js
@@ -0,0 +1,63 @@
+let ball
+let ball_player
+let colicion
+let color
+let id=0
+var lista=[];
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	let pw = 80
+	let ph = 160
+	let py = floor(windowHeight / 2) - floor(ph / 2)
+	
+}
+
+function draw() {	
+	background('#33FFE3')
+	
+	lista.forEach(function(i){
+    	ball_player=i;
+    	ball_player.draw();
+    	ball_player.move();
+    	
+    	lista.forEach(function(i){
+			colicion=i;
+
+			if (ball_player.getId != colicion.getId) {
+				
+				if ((ball_player.pX>=colicion.pX && ball_player.pX<=colicion.pX+20) && (ball_player.pY>=colicion.pY && ball_player.pY<=colicion.pY+20)) {
+    				ball_player.changeDirection();
+    				colicion.changeDirection()
+    				console.log('colicion id= '+ ball_player.getId + " " + 'con id= ' + colicion.getId)
+    				console.log('colicion x= '+ ball_player.pX + " " + 'con y= ' + ball_player.pY + '//// x= '+ colicion.pX + " " +'con y= ' + colicion.pY)
+    			
+    			}else{
+
+    			}
+			}else{
+
+			}
+    		
+		});
+    	
+	});
+}
+
+function mousePressed () {
+	
+	ball = new Ball(floor(Math.random() * windowWidth),floor(Math.random() * windowHeight),getRandomColor(),floor(windowWidth),floor(windowHeight ),id)
+	lista.push(ball);
+	
+	id+=1
+	
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
